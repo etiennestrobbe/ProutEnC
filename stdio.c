7883a638fa177a9aea1dfaad256b68011be6b1df
Etienne Strobbe <etienne.strobbe@gmail.com>
2014-05-28 14:51:21 +0200

    chépas

--- stdio.c
+++ stdio.c
@@ -1,10 +1,10 @@
 #include "stdio.h"
 
-int _filbuf(FILE *f)
-{
+int _filbuf(FILE *f){
 	// controle 
 	//- allouer eventuellement un buffer
 	//- verifier que f est en lecture
+	puts ("PROUT");
 	int n,c;
 	n=read(f->_file, &c, 1);
 	f->_cnt=0; // pour garantir que le prochain getC qu'on va faire, X
